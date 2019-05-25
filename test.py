b177a9e5e3fa3a95af55fc440ad8a48eb943b765
Paul <38810529+paulboxwell@users.noreply.github.com>
2019-05-25 01:33:23 +0100

    initial code

---
+++ test.py
@@ -0,0 +1,5 @@
+print("Hello Everyone \nI am hoping to learn all about Git, GitHub and Visual Studio Code.")
+while(True):
+    
+    name = input("What is your name?")
+    print("Nice to meet you " + name)
